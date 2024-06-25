7c4028d1c5d6579614ba170425f54a116d2f39c9
Greenfoot5 <greenfoot89@gmail.com>
2024-06-25 12:26:57 +0100

    Moved files to old

---
+++ old-resources/data/skills.js
@@ -0,0 +1,196 @@
+let _skills = {
+    theorder: {
+        faith: {
+            effect: effectTypes.income,
+            effectvalue: 0.205,
+            baseprice: 1,
+            io: {
+                html: "faith", display: "Faith", import: "faith"
+            }
+        },
+        zeal: {
+            effect: effectTypes.allprice,
+            effectvalue: 0.12,
+            baseprice: 1,
+            io: {
+                html: "zeal", display: "Zeal", import: "zeal"
+            }
+        },
+        devotion: {
+            effect: effectTypes.happiness,
+            effectvalue: 0.135,
+            baseprice: 1,
+            io: {
+                html: "devotion", display: "Devotion", import: "devotion"
+            }
+        },
+        fervour: {
+            effect: effectTypes.shopprice,
+            effectvalue: 0.0135,
+            baseprice: 1,
+            io: {
+                html: "fervour", display: "Fervour", import: "fervour"
+            }
+        }
+    },
+    fundamentals: {
+        productivity: {
+            effect: effectTypes.income,
+            effectvalue: 0.03,
+            baseprice: 1,
+            io: {
+                html: "productivity", display: "Productivity", import: "productivity"
+            }
+        },
+        concentration: {
+            effect: effectTypes.skilleffects,
+            effectvalue: 0.01,
+            baseprice: 1,
+            io: {
+                html: "concentration", display: "Concentration", import: "concentration"
+            }
+        },
+        bargaining: {
+            effect: effectTypes.allprice,
+            effectvalue: 0.01,
+            baseprice: 1,
+            io: {
+                html: "bargaining", display: "Bargaining", import: "bargaining"
+            }
+        },
+        meditation: {
+            effect: effectTypes.happiness,
+            effectvalue: 0.01,
+            baseprice: 1,
+            io: {
+                html: "meditation", display: "Meditation", import: "meditation"
+            }
+        }
+    },
+    combat: {
+        strength: {
+            effect: effectTypes.attackpower,
+            effectvalue: 0.015,
+            baseprice: 1,
+            io: {
+                html: "strength", display: "Strength", import: "strength"
+            }
+        },
+        musclememory: {
+            effect: effectTypes.attackduration,
+            effectvalue: 0.01,
+            baseprice: 1,
+            io: {
+                html: "musclememory", display: "Muscle Memory", import: "muscle memory"
+            }
+        },
+        battletactics: {
+            effect: effectTypes.enattackduration,
+            effectvalue: 0.01,
+            baseprice: 1,
+            io: {
+                html: "battletactics", display: "Battle Tactics", import: "battle tactics"
+            }
+        }
+    },
+    magic: {
+        manacontrol: {
+            effect: effectTypes.allprice,
+            effectvalue: 0.01,
+            baseprice: 1,
+            io: {
+                html: "manacontrol", display: "Mana Control", import: "mana control"
+            }
+        },
+        lifeessence: {
+            effect: effectTypes.fanatacismgain,
+            effectvalue: 0.02,
+            baseprice: 1,
+            io: {
+                html: "lifeessence", display: "Life Essence", import: "life essence"
+            }
+        },
+        resilience: {
+            effect: effectTypes.attackresistance,
+            effectvalue: 0.005,
+            baseprice: 1,
+            io: {
+                html: "resilience", display: "Resilience", import: "resilience"
+            }
+        },
+        materialism: {
+            effect: effectTypes.greed,
+            effectvalue: 0.04,
+            baseprice: 1,
+            io: {
+                html: "materialism", display: "Materialism", import: "materialism"
+            }
+        }
+    },
+    darkmagic: {
+        fanaticaldevotion: {
+            effect: effectTypes.happiness,
+            effectvalue: 0.01,
+            baseprice: 1,
+            io: {
+                html: "fanaticaldevotion", display: "Fanatical Devotion", import: "fanatical devotion"
+            }
+        },
+        ardentbelief: {
+            effect: effectTypes.fanatacismgain,
+            effectvalue: 0.03,
+            baseprice: 1,
+            io: {
+                html: "ardentbelief", display: "Ardent Belief", import: "ardent belief"
+            }
+        },
+        zealousconviction: {
+            effect: effectTypes.allprice,
+            effectvalue: 0.01,
+            baseprice: 1,
+            io: {
+                html: "zealousconviction", display: "Zealous Conviction", import: "zealous conviction"
+            }
+        },
+        extremepiety: {
+            effect: effectTypes.happiness,
+            effectvalue: 0.01,
+            baseprice: 1,
+            io: {
+                html: "extremepiety", display: "Extreme Piety", import: "extreme piety"
+            }
+        },
+        absolutefaith: {
+            effect: effectTypes.fanatacismgain,
+            effectvalue: 0.01,
+            baseprice: 1,
+            io: {
+                html: "absolutefaith", display: "Absolute Faith", import: "absolute faith"
+            }
+        },
+        devoutmastery: {
+            effect: effectTypes.income,
+            effectvalue: 0.002,
+            baseprice: 1,
+            io: {
+                html: "devoutmastery", display: "Devout Mastery", import: "devout mastery"
+            }
+        },
+        doggedperseverance: {
+            effect: effectTypes.shopprice,
+            effectvalue: 0.003,
+            baseprice: 1,
+            io: {
+                html: "doggedperseverance", display: "Dogged Perseverance", import: "dogged perseverance"
+            }
+        },
+        blazingfervour: {
+            effect: effectTypes.greed,
+            effectvalue: 0.0027,
+            baseprice: 1,
+            io: {
+                html: "blazingfervour", display: "Blazing Fervor", import: "blazing fervor"
+            }
+        }
+    }
+};
