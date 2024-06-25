c74b3e749b52e48de86d914daee5ed38baa3a438
Greenfoot5 <greenfoot89@gmail.com>
2024-06-25 22:04:23 +0100

    Cleanup

--- src/lib/components/Money.svelte
+++ src/lib/components/Money.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
     export let amount = 0;
 
-    $: split = amount.toString().split('')
-
     $: copper = amount % 1e2;
     $: silver = Math.floor((amount % 1e4 - copper) / 1e2);
     $: gold = Math.floor((amount % 1e6 - silver) / 1e4);
@@ -14,30 +12,30 @@
     $: diamond = Math.floor((amount - topaz) / 1e16);
 </script>
 
-    {#if diamond > 0}
-        <span class="pkm-diamond">{diamond}d </span>
-    {/if}
-    {#if topaz > 0}
-        <span class="pkm-topaz">{topaz}t </span>
-    {/if}
-    {#if ruby > 0}
-        <span class="pkm-ruby">{ruby}r </span>
-    {/if}
-    {#if amethyst > 0}
-        <span class="pkm-amethyst">{amethyst}a </span>
-    {/if}
-    {#if emerald > 0}
-        <span class="pkm-emerald">{emerald}e </span>
-    {/if}
-    {#if platinum > 0}
-        <span class="pkm-platinum">{platinum}p </span>
-    {/if}
-    {#if gold > 0}
-        <span class="pkm-gold">{gold}g </span>
-    {/if}
-    {#if silver > 0}
-        <span class="pkm-silver">{silver}s </span>
-    {/if}
-    {#if copper > 0}
-        <span class="pkm-copper">{copper}c </span>
-    {/if}
+{#if diamond > 0}
+    <span class="pkm-diamond">{diamond}d </span>
+{/if}
+{#if topaz > 0}
+    <span class="pkm-topaz">{topaz}t </span>
+{/if}
+{#if ruby > 0}
+    <span class="pkm-ruby">{ruby}r </span>
+{/if}
+{#if amethyst > 0}
+    <span class="pkm-amethyst">{amethyst}a </span>
+{/if}
+{#if emerald > 0}
+    <span class="pkm-emerald">{emerald}e </span>
+{/if}
+{#if platinum > 0}
+    <span class="pkm-platinum">{platinum}p </span>
+{/if}
+{#if gold > 0}
+    <span class="pkm-gold">{gold}g </span>
+{/if}
+{#if silver > 0}
+    <span class="pkm-silver">{silver}s </span>
+{/if}
+{#if copper > 0}
+    <span class="pkm-copper">{copper}c </span>
+{/if}
